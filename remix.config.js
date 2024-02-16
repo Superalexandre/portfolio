7766360fe13d6178f24cc41ccb8ad5bf7166de54
Alexandre Renard <superalexandre0@gmail.com>
2024-02-16 19:35:17 +0100

    ci: 🔧 Update remix config

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
     ignoredRouteFiles: ["**/.*"],
-    // appDirectory: "app",
-    // assetsBuildDirectory: "public/build",
-    // publicPath: "/build/",
-    // serverBuildPath: "build/index.js",
+    appDirectory: "app",
+    assetsBuildDirectory: "public/build",
+    publicPath: "/build/",
+    serverBuildPath: "build/index.js",
     serverDependenciesToBundle: [
         "remix-i18next",
     ],
     serverModuleFormat: "esm",
     serverPlatform: "node",
-    cacheDirectory: "./node_modules/.cache/remix",
+    // cacheDirectory: "./node_modules/.cache/remix",
+    serverMinify: true,
 }
