7c9c5801b481209896cb193eac591430fbf3c87e
Alexandre Renard <superalexandre0@gmail.com>
2024-02-22 18:45:08 +0100

    fix: 🐛 Fix import error

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { serve } from "@hono/node-server"
 import { serveStatic } from "@hono/node-server/serve-static"
 import { Hono } from "hono"
-import { remix } from "remix-hono"
+import { remix } from "remix-hono/handler"
 
 import "dotenv/config"
 
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-declare module "remix-hono" {
+declare module "remix-hono/handler" {
     import { Hono } from "hono"
     import { BuildOptions } from "remix"
     export function remix(options: BuildOptions): Hono.Middleware
