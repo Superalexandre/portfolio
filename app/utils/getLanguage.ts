583422cc5ddad593a9842ddc039dd4d007daf3a7
Alexandre Renard <superalexandre0@gmail.com>
2024-03-03 14:27:16 +0100

    feat: ✨ Add a function to get the closet language from the headers

--- app/utils/getLanguage.ts
+++ app/utils/getLanguage.ts
@@ -28,15 +28,36 @@ function getLanguage(request: Request) {
 
     // Get the language from the headers
     const headers = request.headers.get("Accept-Language")
-    const languages = parseAcceptLanguage(headers || "") || []
-    const validLanguages = languages.filter((languageHeader) => i18next.options.i18next.supportedLngs.includes(languageHeader))
+    const languagesHeader = parseAcceptLanguage(headers || "") || []
+    const validLanguages = languagesHeader.filter((languageHeader) => i18next.options.i18next.supportedLngs.includes(languageHeader))
 
     if (!hasChanged && validLanguages.length > 0) {
         hasChanged = true
         locale = validLanguages[0]
     }
 
+    // Get the language from the closest language in the headers
+    const closestLanguage = getClosetLanguage(languagesHeader)
+    if (!hasChanged && languagesHeader.length > 0 && closestLanguage) {
+        hasChanged = true
+        locale = closestLanguage
+    }
+
     return locale
+}
+
+function getClosetLanguage(languagesHeader: string[]) {
+    const supportedLanguages: string[] = i18next.options.i18next.supportedLngs
+    
+    if (!supportedLanguages || supportedLanguages.length === 0) return null
+
+    for (const languageHeader of languagesHeader) {
+        for (const supportedLanguage of supportedLanguages) {
+            if (languageHeader.startsWith(supportedLanguage)) {
+                return supportedLanguage
+            }
+        }
+    }
 }
 
 export default getLanguage
