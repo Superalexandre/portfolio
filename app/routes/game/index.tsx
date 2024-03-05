272848ee168e97abd48b0b8f068f951919720e80
Alexandre Renard <superalexandre0@gmail.com>
2024-03-05 20:13:21 +0100

    feat: ✨ Improve the game

--- app/routes/game/index.tsx
+++ app/routes/game/index.tsx
@@ -1,42 +1,72 @@
-import { useEffect, useRef } from "react"
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
+import { useEffect, useRef, useState } from "react"
+import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from "react-zoom-pan-pinch"
 import { v4 as uuid } from "uuid"
 
 const MARGIN = 200
+const STATIONS_NUMBER = 2
+
+const CANVAS_WIDTH = 2_000
+const CANVAS_HEIGHT = 1_000
+
+type Point = {
+    x: number
+    y: number
+    id: string
+}
 
 export default function Index() {
+    const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
-    const pointsRef = useRef<{ x: number; y: number; id: string }[]>([])
+    const pointsRef = useRef<Point[]>([])
+    const [clickedPoints, setClickedPoints] = useState<Point[]>([])
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas?.getContext("2d")
-
+        
         const handleCanvasClick = (event: MouseEvent) => {
-            console.log("click")
+            if (!context || !canvas) return
 
-            const { offsetX, offsetY } = event
-            const clickedPoint = pointsRef.current.find((point) => {
-                offsetX >= point.x &&
-                offsetX <= point.x + 10 &&
-                offsetY >= point.y &&
-                offsetY <= point.y + 10
+            const rect = canvas.getBoundingClientRect()
+            const x = event.clientX - rect.left
+            const y = event.clientY - rect.top
+
+            const clickedPoint = pointsRef.current.find(point => {
+                const pointX = point.x - rect.left
+                const pointY = point.y - rect.top
+
+                const isSameX = x >= pointX && x <= pointX + 10
+                const isSameY = y >= pointY && y <= pointY + 10
+
+                console.log({
+                    x, y,
+                    pointX: pointX,
+                    pointY: pointY,
+                    isSameX,
+                    isSameY,
+                })
+
+                return isSameX && isSameY
             })
 
-            if (context && clickedPoint) {
-                pointsRef.current.push(clickedPoint)
-                drawLines({ points: pointsRef.current, context })
+            if (clickedPoint) {
+                console.log("clickedPoint", clickedPoint)
+                
+                setClickedPoints(prev => [...prev, clickedPoint])
             }
+
         }
 
         if (canvas && context) {
             canvas.addEventListener("click", handleCanvasClick)
 
-            drawPoints({ context })
+            const points = drawPoints({ context })
+            pointsRef.current = points
         }
 
         return () => {
-            canvas?.removeEventListener("click", handleCanvasClick)
+            if (canvas) canvas.removeEventListener("click", handleCanvasClick)
+            if (context) context.clearRect(0, 0, context.canvas.width, context.canvas.height)
         }
     }, [])
 
@@ -45,11 +75,12 @@ export default function Index() {
             initialScale={1}
             initialPositionX={0}
             initialPositionY={0}
+            ref={transformComponentRef}
         >
             <TransformComponent wrapperClass="bg-green-200">
                 <canvas
-                    width={2_000}
-                    height={1_000}
+                    width={CANVAS_WIDTH}
+                    height={CANVAS_HEIGHT}
                     ref={canvasRef}
                     className="h-screen w-screen"
                 />
@@ -58,10 +89,10 @@ export default function Index() {
     )
 }
 
-const drawPoints = ({ number = 10, context }: { number?: number, context: CanvasRenderingContext2D }) => {
+const drawPoints = ({ number = STATIONS_NUMBER, context }: { number?: number, context: CanvasRenderingContext2D }) => {
     const getPosition = () => {
-        const x = Math.floor(Math.random() * (context.canvas.width - MARGIN))
-        const y = Math.floor(Math.random() * (context.canvas.height - MARGIN))
+        const x = Math.floor(Math.random() * (CANVAS_WIDTH - MARGIN) + MARGIN)
+        const y = Math.floor(Math.random() * (CANVAS_HEIGHT - MARGIN) + MARGIN)
 
         return { x, y }
     }
@@ -76,11 +107,7 @@ const drawPoints = ({ number = 10, context }: { number?: number, context: Canvas
         context.rect(x, y, 10, 10)
         context.fill()
 
-        return {
-            x,
-            y,
-            id
-        }
+        return { x, y, id }
     })
 
     return points
@@ -101,3 +128,19 @@ const drawLines = ({ points, context, }: { points: { x: number; y: number; id: s
         }
     })
 }
+
+/*
+const positionFromStyle = (style: string) => {
+    const regex = /translate\(([-+]?\d*\.?\d+)px,\s*([-+]?\d*\.?\d+)px\)\s*scale\(([-+]?\d*\.?\d+)\)/gm
+
+    const match = regex.exec(style)
+
+    if (match) {
+        const [, x, y, scale] = match
+        
+        return { x: +x, y: +y, scale: +scale }
+    }
+
+    return { x: 0, y: 0, scale: 1 }
+}
+*/ 
