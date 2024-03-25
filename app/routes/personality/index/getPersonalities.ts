73504f59e3fb8891e2d778ee718b59b59198212e
Alexandre Renard <superalexandre0@gmail.com>
2024-03-25 20:42:34 +0100

    fix: 🐛 Wrong path

--- app/routes/personality/index/getPersonalities.ts
+++ app/routes/personality/index/getPersonalities.ts
@@ -6,7 +6,8 @@ import QuestionFile from "@/types/personality/QuestionFile"
 
 export function getPersonalities() {
     const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    const pathJson = path.join(__dirname, "..", "app", "routes", "personality", "questions")
+    // const pathJson = path.join(__dirname, "..", "app", "routes", "personality", "questions")
+    const pathJson = path.join(__dirname, "..", "questions")
 
     const files = fs.readdirSync(pathJson).map(file => {
         const fileContent = fs.readFileSync(path.join(pathJson, file), "utf-8")
