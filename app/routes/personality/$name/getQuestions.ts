24f97b6096b66ace96a3736db103e174c61631dd
Alexandre Renard <superalexandre0@gmail.com>
2024-03-25 20:57:16 +0100

    fix: 🐛 Fix path and url (again)

--- app/routes/personality/$name/getQuestions.ts
+++ app/routes/personality/$name/getQuestions.ts
@@ -1,12 +1,10 @@
 import fs from "fs"
 import path from "path"
-import { fileURLToPath } from "url"
 
 import QuestionFile from "@/types/personality/QuestionFile"
 
 export function getQuestions(name: string) {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    const pathJson = path.join(__dirname, "..", "app", "routes", "personality", "questions", `${name}.json`)
+    const pathJson = path.join("public", "questions", `${name}.json`)
 
     const hasFile = fs.existsSync(pathJson)
     if (!hasFile) return null
--- app/routes/personality/index/getPersonalities.ts
+++ app/routes/personality/index/getPersonalities.ts
@@ -1,13 +1,10 @@
 import fs from "fs"
 import path from "path"
-import { fileURLToPath } from "url"
 
 import QuestionFile from "@/types/personality/QuestionFile"
 
 export function getPersonalities() {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    // const pathJson = path.join(__dirname, "..", "app", "routes", "personality", "questions")
-    const pathJson = path.join(__dirname, "..", "questions")
+    const pathJson = path.join("public", "questions")
 
     const files = fs.readdirSync(pathJson).map(file => {
         const fileContent = fs.readFileSync(path.join(pathJson, file), "utf-8")
